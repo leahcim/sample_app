87343624c447a9ad4b4ae1ad7f9336d7efc52c48
Michael <mnichau@yahoo.de>
2011-02-15 19:24:03 +0000

    Moved logo code to a helper; added link tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Clickable logo
+  def logo
+    image_tag('logo.png', :alt => 'Sample App', :class => 'round')
+  end
+
   # Return title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
